20c6b96d2d4def2e70bd7bfee66b4d4d682bdc50
Alexander Cerutti <cerutti.alexander@gmail.com>
2018-02-12 18:18:42 +0100

    Added tests for functions of keyboard types Added an example bot (have to pass token via argv) which works with the library

---
+++ test/bot/index.js
@@ -0,0 +1,63 @@
+const { InlineKeyboard, ReplyKeyboard } = require("../..");
+const TelegramBot = require("node-telegram-bot-api");
+
+console.log(process.argv);
+
+if (process.argv.length < 3) {
+	throw new Error("To test this bot, please pass a bot-token to the application.");
+}
+
+const token = process.argv[2];
+const bot = new TelegramBot(token, { polling: true });
+let isRKOpen = false;
+
+const rk = new ReplyKeyboard();
+const ik = new InlineKeyboard();
+
+rk
+	.addRow("1:1 button", "1:2 button")
+	.addRow("2:1 button", "2:2 button");
+
+ik
+	.addRow(
+		{ text: "1:1 button", callback_data: "Works!" },
+		{ text: "1:2 button", callback_data: "Works!" }
+	)
+	.addRow(
+		{ text: "2:1 button", callback_data: "Works!" },
+		{ text: "2:2 button", callback_data: "Works!" }
+	);
+
+
+function hasBotCommands(entities) {
+	if (!entities || !(entities instanceof Array)) {
+		return false;
+	}
+
+	return entities.some(e => e.type === "bot_command");
+}
+
+bot.onText(/\/replyKeyboard/i, function(msg) {
+	bot.sendMessage(msg.from.id, "This is a message with a reply keyboard. Click on one of the buttons to close it.", rk.open())
+		.then(function() {
+			isRKOpen = !isRKOpen;
+		});
+});
+
+bot.on("message", function(msg) {
+	if (!hasBotCommands(msg.entities) && isRKOpen) {
+		bot.sendMessage(msg.from.id, "Good! I'm closing the replyKeyboard.", rk.close());
+		isRKOpen = !isRKOpen;
+	}
+});
+
+bot.onText(/\/inlineKeyboard/i, function(msg) {
+	bot.sendMessage(msg.from.id, "This is a message with an inline keyboard.", ik.export());
+});
+
+bot.on("callback_query", function(query) {
+	bot.answerCallbackQuery(query.id, { text: "Action received!" })
+	.then(function() {
+		bot.sendMessage(query.from.id, "Hey there! You clicked on an inline button! ;) So, as you saw, the support library works!");
+	});
+});
---
+++ test/replyKeyboard.js
@@ -0,0 +1,63 @@
+const main = require("..");
+const assert = require("assert");
+/*
+ * Tests for Reply Keyboards
+ */
+
+describe("Reply Keyboards tests", function() {
+	// Some dummy data
+	let firstLoad = ["This is a field.", "This is another field"];
+	let secondLoad = ["Meh"];
+	let thirdLoad = ["Third load button"];
+
+	describe(".addRow(...keys)", function() {
+		let keyboard;
+
+		beforeEach("init keyboard", function() {
+			keyboard = new main.ReplyKeyboard();
+		});
+
+		it("Should add elements to a row and return the right number of rows via .extract() method", function() {
+			keyboard
+				.addRow(...firstLoad)
+				.addRow(...secondLoad);
+
+			assert.equal(keyboard.extract(keyboard.export()).length, 2);
+		});
+
+		it("Should have a length property after .addRow(), which returns the amount of rows", function() {
+			assert.equal(keyboard.addRow(...thirdLoad).length, 1);
+		});
+	});
+
+	describe(".open()", function() {
+		let keyboard = new main.ReplyKeyboard();
+			keyboard
+				.addRow(...firstLoad)
+				.addRow(...secondLoad)
+
+		it("Should return the data structure for telegram", function() {
+			assert.deepEqual(keyboard.open(), {
+				reply_markup: {
+					keyboard: [ firstLoad, secondLoad ],
+				},
+				resize_keyboard: true,
+			});
+		});
+	});
+
+	describe(".close()", function() {
+		let keyboard = new main.ReplyKeyboard();
+			keyboard
+				.addRow(...firstLoad)
+				.addRow(...secondLoad)
+
+		it("Should return the telegram data structure", function() {
+			assert.deepEqual(keyboard.close(), {
+				reply_markup: {
+					remove_keyboard: true
+				}
+			});
+		});
+	});
+});
