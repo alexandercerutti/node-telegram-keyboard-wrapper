d5c5b9cb6f534e52c43cb28ff31d3c9901e60d51
Alexander Cerutti <cerutti.alexander@gmail.com>
2018-10-27 00:10:56 +0200

    Moved ReplyKeyboard to typescript, with newly designed open and close methods

---
+++ src/reply_keyboard.ts
@@ -0,0 +1,39 @@
+import { VisualKeyboard } from "./reply_markup";
+import { RKOpenOptions, RMSelective } from "./model";
+import { KeyboardButton } from "node-telegram-bot-api";
+
+export class ReplyKeyboard extends VisualKeyboard {
+	constructor(oneKey?: KeyboardButton) {
+		// not passing type to super because it depends from the method (open or close)
+		super();
+
+		if (oneKey) {
+			this.addRow(oneKey);
+		}
+	}
+
+	/**
+	 * Returns a structure to open a keyboard
+	 * @param options - Options included in ReplyKeyboardMarkup
+	 * @see https://core.telegram.org/bots/api#replykeyboardmarkup
+	 */
+
+	open(options: RKOpenOptions = {}) {
+		this.setKeyboardType("keyboard");
+
+		return Object.assign(this.build(), options);
+	}
+
+	/**
+	 * Returns a structure to close the keyboard
+	 * @param options - Options included in ReplyKeyboardRemove
+	 * @see https://core.telegram.org/bots/api#replykeyboardremove
+	 */
+
+	close(options: RMSelective = {}) {
+		this.setKeyboardType("remove_keyboard");
+		this._override = true;
+
+		return Object.assign(this.build(), options);
+	}
+}
