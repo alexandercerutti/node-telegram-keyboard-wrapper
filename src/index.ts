a61cde856d8123dd7331dce62280b3b5501ab6fc
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-03-07 23:32:38 +0100

    Added more exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { default as ReplyKeyboard } from "./ReplyKeyboard";
 export { default as KeyboardButton } from "./KeyboardButton";
+export { default as InlineKeyboard } from "./InlineKeyboard";
 export { default as InlineKeyboardButton } from "./InlineKeyboardButton";
+export { default as Row } from "./Row";
