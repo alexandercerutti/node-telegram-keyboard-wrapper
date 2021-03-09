b14748caad394c2eafb14db92c78ca7e84536142
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-03-09 22:29:56 +0100

    Removed forgotten call from Row

--- src/Row.ts
+++ src/Row.ts
@@ -5,7 +5,6 @@ type RowTypes = InlineKeyboardButton | KeyboardButton;
 
 export default class Row<R extends RowTypes> extends Array<R> {
 	getJSON() {
-		return this.map
-			.call(this, (element: RowTypes) => element.getJSON());
+		return this.map((element: RowTypes) => element.getJSON());
 	}
 }
